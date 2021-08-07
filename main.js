7f498eb1d57fd022c43e9b6f0137d3dc6e7ce475
ermias-mangisto <ermiasmangisto@gmail.com>
2021-08-07 23:03:52 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,178 @@
+
+
+// var num1=Number(prompt("number"))
+// var num2=Number(prompt("number"))
+// var num3=Number(prompt("number"))
+// if(num1>num2 && num2>num3){
+//     console.log("downward order");
+// }
+// else if(num3>num2 && num2>num1){
+// console.log("upward order");
+// }
+// else{console.log("no order")}
+
+// ..............................................................
+
+// var ages=[1,2,3,4,5,6]
+// var min=ages[0];
+// for (var i=0;i<ages.length;i++){
+//     if (ages[i]>=min){
+//         min=ages[i]
+//     }
+    
+// }console.log(min);
+// ..............................................................
+
+
+// for (var i=0;i<ages.length;i++){
+//     if (ages[i]<=min){
+//         min=ages[i]
+//     }
+    
+// }console.log(min);
+// ..............................................................
+
+// var sum=0;
+// for(var i=0;i<ages.length;i++){
+     
+//     sum=sum+ages[i]
+// }
+// console.log(sum);
+
+// ..............................................................
+
+
+// var avg=0;
+// for(var i=0;i<ages.length;i++){
+//     avg=(avg+ages[i])
+// }
+// avg=avg/ages.length
+// console.log(avg);
+// ..............................................................
+
+// var userNum=Number(prompt("number"))
+// for(var i=0;i<ages.length;i++){
+//     if(userNum==ages[i]){
+//         alert("good guess")
+//     }
+// }
+// ..............................................................
+
+// var times=1;
+// for(var i=0;i<ages.length;i++){
+//     times=times*ages[i]
+// }
+// console.log(times);
+// ..............................................................
+
+// for(var i=0;i<ages.length;i++){
+//     for(var j=0;j<10;j++){
+
+//         console.log(ages[i])
+//     }
+// }
+// ..............................................................
+
+// var numbers=[7,8,9,6,56,54]
+// var stuff=["ggg","hkkh","khkh","koko"]
+
+// ...........................................................
+
+// var numbers=[5,6,7,4,56,7,89,87]
+// var num=numbers[0]
+// numbers[0]=numbers[1]
+// numbers[1]=num
+// var num1=numbers[0]
+// numbers[0]=numbers[7]
+// numbers[7]=num1
+
+
+// console.log(numbers);
+
+// ......................................................
+
+// var numbers=[5,3,7,8,11]
+
+// var sum=0;
+// var avg;
+// for(var i=0;i<numbers.length;i++){
+     
+//     sum=sum+numbers[i]
+// }
+// avg=sum/numbers.length;
+// console.log(sum);
+// console.log(avg);
+// ..............................................................
+
+// var names=["avi","yosi","dan","ron","kim"]
+// for(var i=0;i<names.length;i++){
+// console.log(names[i]);
+// }
+// ...................................................................
+// var names=[]
+// for(var i=0;i<5;i++){
+//     var input=prompt("name")
+//     names.push(input)
+// }
+// console.log(names);
+// ...........................................................
+//  var names=["avi","yosi","dan","ron","kim","don", "haim", "bob"]
+//  var input=prompt("name")
+//  for(var i=0;i<names.length;i++){
+//      if(names[i]==input){
+//          alert("good guess")
+//      }
+     
+//  }
+// ..............................................................
+// var numbers=[]
+//  for(var i=0;i<3;i++){
+//      var input=Number(prompt("number"))
+//      if(input){
+//      numbers.push(input)}
+//      else{i--
+//          alert("only numbers try again")
+//      }
+//  }
+// console.log(numbers);
+
+
+// var total=1;
+// for(var i=0;i<numbers.length;i++){
+// total=total*numbers[i]
+
+// }
+// var sum=1;
+// for(var i=0;i<numbers.length;i++){
+// sum=sum+numbers[i]
+
+// }
+// console.log(total)
+// console.log(sum)
+// for(var i=0;i<numbers.length;i++){
+//     if(numbers[i]%2==0){
+//         console.log(`${numbers[i]} even`)
+//     }
+//     else{
+//         console.log(`${numbers[i]} odd`)
+//     }
+// }
+
+// ......................................................................
+
+// for(var i=1;i<11;i++){
+//   for(var j=1;j<11;j++){
+//       console.log(`${i}*${j}=${i*j}`)  
+//   }
+
+// }
+// .........................................................
+//   
+// .............................................................
+// for(var i=1;i<101;i++){
+//     if(i%7!=0){
+//         console.log(i);
+//     }else{
+//         console.log("BOOM");
+//     }
+// }
